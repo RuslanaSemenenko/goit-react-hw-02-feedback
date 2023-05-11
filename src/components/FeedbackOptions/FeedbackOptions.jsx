afab731dc083aab05acc3b25cb3f442aecac6193
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-05-11 20:03:43 +0100

    homework

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      {options.map(option => (
+        <button
+          key={uuidv4()}
+          type="button"
+          name={option}
+          onClick={onLeaveFeedback}
+        >
+          {option}
+        </button>
+      ))}
+    </>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
